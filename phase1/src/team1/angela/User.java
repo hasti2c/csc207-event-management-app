31d4ffa36f059a05f75ed380f3fef8a2a46f0392
AngelaXiang <angelaxiang.ca@gmail.com>
2021-07-03 20:26:27 -0400

    Finished User class methods

--- phase1/src/team1/angela/User.java
+++ phase1/src/team1/angela/User.java
@@ -1,5 +1,6 @@
 package team1.angela;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -8,7 +9,6 @@ public class User {
      * The users of the system
      */
     // === Instance Variables ===
-    private Map<String, String> userInfo;
     private String username;
     private String password;
     private String userEmail;
@@ -20,9 +20,45 @@ public class User {
     // === Representation Invariants ===
     // Not sure yet
     // === Methods ===
-    public User(Map<String, String> info, userType type) {
-
+    public User(String username, String password, String userEmail, userType type) {
+        this.username = username;
+        this.password = password;
+        this.userEmail = userEmail;
+        userEvents = new ArrayList<>();
+        loggedIn = false;
     }
-    // need to add getters and setters for everything except userEvents
+    // === Getters ===
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getUserEmail() {
+        return userEmail;
+    }
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+    public List<Event> getUserEvents() {
+        return userEvents;
+    }
+    // === Setters ===
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+    public void setUserEvents(List<Event> userEvents) {
+        this.userEvents = userEvents;
+    }
 
 }
--- phase1/src/team1/angela/UserManager.java
+++ phase1/src/team1/angela/UserManager.java
@@ -1,5 +1,37 @@
 package team1.angela;
 
+import java.util.List;
+
 public class UserManager {
+    /**
+     * Manages the Users in the system
+     */
+    // === Class Variables ===
+    public static List<User> userList;
+    // === Instance Variables ===
+    // === Methods ===
+    // Need to finalize details about what to include for user creation before finishing this
+    public void createUser() {
+
+    }
+    public void deleteUser(User user) {
+        // remove User from list of users
+    }
+    public boolean logIn(String username, String password) {
+        // returns true if successfully logged in, false if otherwise (like if password is wrong)
+        // updates the loggedIn boolean to True
+        return true;
+    }
+    public boolean logOut(String username, String password) {
+        // returns true if successfully logged out, false if otherwise (like if password is wrong)
+        return true;
+    }
+    public boolean updateUser(User user){
+        // returns true if user is updated successfully
+        // access getters and setters of useUser class
+        // User needs to be logged in
+        return true;
+    }
+
 
 }
