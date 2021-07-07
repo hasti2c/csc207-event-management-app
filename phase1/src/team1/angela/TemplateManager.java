c441eb29da5288aff55da9a93a4ee308c2a55d7d
AngelaXiang <angelaxiang.ca@gmail.com>
2021-07-07 11:11:24 -0400

    Updated TemplateManager

--- phase1/src/team1/angela/TemplateManager.java
+++ phase1/src/team1/angela/TemplateManager.java
@@ -19,15 +19,15 @@ public class TemplateManager {
         // returns true if name is updated successfully
         return true;
     }
-//    public Template generateTemplateFromFile(String templateFileName) {
-//        File file = new File(templateFileName);
-//        if(file.exists()){
-//            return new Template(templateFileName);
-//        }
-//        return null;
-//    }
-//    public Template createNewTemplate(String templateFileName) {
-//        Template obj = new Template(templateFileName);
-//        return obj;
-//    }
+    public Template generateTemplateFromFile(String templateFileName) {
+        File file = new File(templateFileName);
+        if(file.exists()){
+            return new Template(templateFileName);
+        }
+        return null;
+    }
+    public Template createNewTemplate(String templateFileName) {
+        Template obj = new Template(templateFileName);
+        return obj;
+    }
 }
