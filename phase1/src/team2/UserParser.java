cb4cf3626453af68f63bb673db9352ed58a2194f
Hasti Toossi <hasti.toossi@mail.utoronto.ca>
2021-07-07 10:22:11 -0400

    EventParser & UserParser v1.0

--- phase1/src/team2/UserParser.java
+++ phase1/src/team2/UserParser.java
@@ -1,23 +1,65 @@
 package team2;
 
+import team1.angela.Event;
 import team1.angela.User;
 
+import java.io.*;
+
+// TODO source https://www.tutorialspoint.com/java/java_serialization.htm
+// TODO exceptions
+// TODO boolean return value
+// TODO ask for username to be okay in file name, and not include dashes.
+// TODO ask for transient fields
 public class UserParser {
-    private String path;
+    private final String path;
 
     public UserParser(String path) {
-
+        this.path = path;
     }
 
     public User getUser(String username) {
-        return null;
+        try {
+            String filePath = getFilePath(username);
+            FileInputStream input = new FileInputStream(filePath);
+            ObjectInputStream deserializer = new ObjectInputStream(input);
+
+            User user = (User) deserializer.readObject();
+            input.close();
+            deserializer.close();
+            return user;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public void saveUser(User user) {
+        try {
+            String filePath = getFilePath(user.getUsername());
+            FileOutputStream output = new FileOutputStream(filePath);
+            ObjectOutputStream serializer = new ObjectOutputStream(output);
+
+            serializer.writeObject(user);
+            output.close();
+            serializer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void createUser(User user) {
+        String filePath = getFilePath(user.getUsername());
+        File file = new File(filePath);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        saveUser(user);
+    }
 
+    private String getFilePath(String username) {
+        return path + "/user-" + username + ".ser";
     }
 }
 
