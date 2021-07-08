a38684ed14113802e3e2aece4d189e606763ed34
AngelaXiang <angelaxiang.ca@gmail.com>
2021-07-07 23:21:59 -0400

    Added a getFieldSpecs to TemplateManager and added getters and setters to FieldSpecs.java

--- phase1/src/team1/TemplateManager.java
+++ phase1/src/team1/TemplateManager.java
@@ -1,6 +1,7 @@
 package team1;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TemplateManager {
@@ -20,15 +21,18 @@ public class TemplateManager {
         return true;
     }
 
-    public Template generateTemplateFromFile(String templateFileName) {
-        File file = new File(templateFileName);
-        if(file.exists()){
-            return new Template(templateFileName);
-        }
-        return null;
+    public List<FieldSpecs> getFieldSpecs(String templateName){
+        return new ArrayList<>(); //placeholder
     }
-    public Template createNewTemplate(String templateFileName) {
-        Template obj = new Template(templateFileName);
-        return obj;
-    }
+//    public Template generateTemplateFromFile(String templateFileName) {
+//        File file = new File(templateFileName);
+//        if(file.exists()){
+//            return new Template(templateFileName);
+//        }
+//        return null;
+//    }
+//    public Template createNewTemplate(String templateFileName) {
+//        Template obj = new Template(templateFileName);
+//        return obj;
+//    }
 }
