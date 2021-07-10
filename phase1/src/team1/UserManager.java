b2585caf9ec1a2983721d7a4a0441594c065df5b
ifaz <ifazalam33@gmail.com>
2021-07-10 15:59:14 -0400

    added javadoc and removed duplicate method. Simplified conditional statement  in attendEvent

--- phase1/src/team1/UserManager.java
+++ phase1/src/team1/UserManager.java
@@ -5,10 +5,10 @@ import sun.font.TrueTypeFont;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Manages the Users in the system
+ */
 public class UserManager {
-    /**
-     * Manages the Users in the system
-     */
     // === Class Variables ===
     public static List<User> userList = new ArrayList<>();
     // === Instance Variables ===
@@ -93,7 +93,7 @@ public class UserManager {
      */
     public boolean updatePassword(User user, String newPassword){
         // returns true if user is updated successfully
-        // access getters and setters of useUser class
+        // access getters and setters of User class
         // User needs to be logged in
         if (user.isLoggedIn()){
             user.setPassword(newPassword);
@@ -112,7 +112,7 @@ public class UserManager {
      */
     public boolean updateUsername(User user, String newUsername) {
         // returns true if user is updated successfully
-        // access getters and setters of useUser class
+        // access getters and setters of User class
         // User needs to be logged in
         if (user.isLoggedIn()){
             user.setUsername(newUsername);
@@ -124,14 +124,14 @@ public class UserManager {
     }
 
     /**
-     * Update a users username to the newUsername
-     * @param user The user whose username is to be updated
+     * Update a users email to the newEmail
+     * @param user The user whose email is to be updated
      * @param newEmail The users new email
-     * @return Whether the username was updated successfully
+     * @return Whether the email was updated successfully
      */
     public boolean updateEmail(User user, String newEmail){
         // returns true if user is updated successfully
-        // access getters and setters of useUser class
+        // access getters and setters of User class
         // User needs to be logged in
         if (user.isLoggedIn()){
             user.setUserEmail(newEmail);
@@ -142,6 +142,12 @@ public class UserManager {
         }
     }
 
+    /**
+     * unregister a user from an event
+     * @param user the user that is to be removed from the event
+     * @param event the event that the user will be removed from
+     * @return whether the user is removed from the event successfully
+     */
     public boolean deleteEvent(User user, Event event){
         if (user.getUserEvents().contains(event)) {
             // deleting an event created by the user means they are also not attending it
@@ -153,11 +159,19 @@ public class UserManager {
         else {
             if (user.getAttendEvents().contains(event)) {
                 user.getAttendEvents().remove(event);
+                // the event has one less attendant
+                event.setNumAttendees(event.getNumAttendees() - 1);
             }
         }
         return true;
     }
 
+    /**
+     * create an Event that is hosted by the given User
+     * @param user the User who is hosting the event
+     * @param event the Event that the user is hosting
+     * @return whether the Event has been successfully created
+     */
     public boolean createEvent(User user, Event event){
         // Add this event to the list of events the user has created
         user.getUserEvents().add(event);
@@ -166,8 +180,14 @@ public class UserManager {
         return true;
     }
 
+    /**
+     * Register the user to attend the event
+     * @param user The user who wishes to attend the event
+     * @param event The Event that the user wishes to attend
+     * @return True if the user was able to register for the event. False if the event has no available space.
+     */
     public boolean attendEvent(User user, Event event) {
-        Object maxAttendees = event.getEventDetails.get("max attendees");
+        Object maxAttendees = event.getEventDetails().get("max attendees");
 
         // Register the user under the event since it has no limit
         if (maxAttendees == null) {
@@ -178,16 +198,21 @@ public class UserManager {
 
         // Check if the event has enough space and register the user under the event if so
         else {
-            if ((int) event.getMaxAttendees() != null && event.getNumAttendees() < event.getMaxAttendees()) {
+            if (event.getNumAttendees() < (int) maxAttendees) {
                 user.getAttendEvents().add(event);
                 event.setNumAttendees(event.getNumAttendees() + 1);
                 return true;
             }
-            return false;
         }
+
+        return false;
     }
 
-    // Display list of events
+    /**
+     * Retrieve the events that a user has created or is attending
+     * @param user The user whose created events / attending events are to be retrieved
+     * @return a list of events the user has created or the user is attending
+     */
     public List<Event> getEvents(User user) {
         // return the union of events the user has created and is attending
         List<Event> events = user.getAttendEvents();
@@ -195,8 +220,10 @@ public class UserManager {
         return events;
     }
 
-    // Returns list of usernames
-    // Returns list of usernames
+    /**
+     * Retrieve all usernames that are registered in UserManager
+     * @return a list of all usernames of every User in UserManager's userList
+     */
     public static List<String> getUsernameList() {
         // Go through each user object in userList and add their username attribute to usernameList
         List<String> usernameList = new ArrayList<>();
@@ -221,26 +248,5 @@ public class UserManager {
         }
         // If the loop ends and no users with the matching username are found, return null
         return null;
-    }
-
-    public boolean attendEvent(User user, Event event) {
-        Object maxAttendees = event.getEventDetails.get("max attendees");
-
-        // Register the user under the event since it has no limit
-        if (maxAttendees == null) {
-            user.getAttendEvents().add(event);
-            event.setNumAttendees(event.getNumAttendees() + 1);
-            return true;
-        }
-
-        // Check if the event has enough space and register the user under the event if so
-        else {
-            if ((int) event.getMaxAttendees() != null && event.getNumAttendees() < event.getMaxAttendees()) {
-                user.getAttendEvents().add(event);
-                event.setNumAttendees(event.getNumAttendees() + 1);
-                return true;
-            }
-            return false;
-        }
     }
 }
