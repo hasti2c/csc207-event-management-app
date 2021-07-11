ba3c48ad0a487ed356e86660b3aee621a6294578
Tony <tonyyqhe@gmail.com>
2021-07-10 23:52:44 -0400

    Modified createEvent

--- phase1/src/team2/SystemController.java
+++ phase1/src/team2/SystemController.java
@@ -1,20 +1,22 @@
 package team2;
 
+
 import team1.angela.EventManager;
-import team1.angela.FieldSpecs;
+
 import team1.angela.TemplateManager;
 import team1.angela.UserManager;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
+
 
 public class SystemController {
     private final UserManager userManager;
     private final EventManager eventManager;
     private final TemplateManager templateManager;
     private final Presenter presenter;
+    private final InputParser inputParser;
     private final Map<String, List<String>> menuMap = new HashMap<>();
 
     public SystemController() {
@@ -22,6 +24,7 @@ public class SystemController {
         this.eventManager = new EventManager();
         this.templateManager = new TemplateManager();
         this.presenter = new Presenter();
+        this.inputParser = new InputParser();
         this.run();
     }
 
@@ -33,44 +36,25 @@ public class SystemController {
         // if login, then give login credentials and check if they are correct, if they are, then proceed to main menu
     }
     // add helper methods down here
-    private Object reqUserInput(String reqName, String reqType) {
-        // this needs to be extended but i was told in phase 1 we would only be dealing with Strings as type requirements
-
-        Scanner reader = new Scanner(System.in);
-
-        this.presenter.printText("Enter a " + reqName + "(" + reqType +"):"); //I do not want to directly print cause presenter should handle all ui
-
-        String userInput = reader.nextLine();
-        while (true) {
-            if (reqType.equals("int")) {
-                try {
-                    return Integer.parseInt(userInput);
-                } catch (NumberFormatException e) {
-                    this.presenter.printText("Enter " + reqType);
-                }
-            }
-            else if (reqType.equals("String")) {
-                return userInput;
-            }
-        }
-
-    }
 
     private void createEvent(String templateString){
         String newEventID = this.eventManager.createEvent(templateString, /* somehow get user*/ ); //i will  assume an id is returned, not full Event
 
-        List<FieldSpecs> field_specs = this.templateManager.getFieldSpecs(templateString);
-        Map<String, Object> userFields = new HashMap<String, Object>();
-
-        for (FieldSpecs fieldData: field_specs) {
-            Object user_input = this.reqUserInput(fieldData.getFieldName(), fieldData.getDataType());
-
-            while (fieldData.getRequired && user_input == null) {
-                user_input = this.reqUserInput(fieldData.getFieldName(), fieldData.getDataType());
+        Map<String, String> fieldMap = this.eventManager.returnFieldNameAndType(newEventID);
+        for (Map.Entry<String, String> entry : fieldMap.entrySet()) {
+            presenter.printText("Enter " + entry.getKey() + "(" + entry.getValue() + "):");
+            String userInput = inputParser.readLine();
+            boolean accepted = false;
+            while (!accepted) {
+                if (eventManager.checkDataValidation(entry.getKey(), userInput)) {
+                    accepted = true;
+                }
+                else {
+                    presenter.printText("Do it right. Enter " + entry.getKey() + "(" + entry.getValue() + "):");
+                    userInput = inputParser.readLine();
+                }
             }
-            userFields.put(fieldData.getFieldName(), this.reqUserInput());
         }
-        this.eventManager.setEventDetails(newEventID, userFields);
     }
 
     private boolean checkLogin(){
