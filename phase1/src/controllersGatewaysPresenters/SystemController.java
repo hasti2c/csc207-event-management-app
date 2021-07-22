137671214854ac89044d5b83dd010d521f7204e2
Hasti Toossi <hasti.toossi@mail.utoronto.ca>
2021-07-22 17:28:35 -0400

    changed system controller initializer + started the mainMenu

--- phase1/src/controllersGatewaysPresenters/SystemController.java
+++ phase1/src/controllersGatewaysPresenters/SystemController.java
@@ -2,8 +2,6 @@ package controllersGatewaysPresenters;
 
 // TODO: ADD LOADING FOR TEMPLATE AND EVENT, USER HAS BEEN DONE CAN BE AN EXAMPLE!
 
-
-// H TODO: Make Initializer less bulky by using helper functions
 // A TODO: Make the Main Run Method
 // A TODO: Make the StartupMenu Method
 // H TODO: Make the mainMenu Method
@@ -11,49 +9,61 @@ package controllersGatewaysPresenters;
 // H TODO: Make the AdminMenu Method
 // A TODO: Make the accountMenu Method
 
-import entitiesAndUseCases.EventManager;
-
-import entitiesAndUseCases.TemplateManager;
-import entitiesAndUseCases.User;
-import entitiesAndUseCases.UserManager;
+import entitiesAndUseCases.*;
 
 import java.util.*;
 
 
 public class SystemController {
+    private final UserController userController;
+    private final EventController eventController;
+    private final Presenter presenter;
+    private final InputParser inputParser;
+
+    private final IGateway<User> userParser;
+    private final IGateway<Event> eventParser;
+    private final IGateway<Template> templateParser;
+
     private final UserManager userManager;
     private final EventManager eventManager;
     private final TemplateManager templateManager;
-    private final Presenter presenter;
-    private final InputParser inputParser;
+
     private final Map<String, List<String>> menuMap = new HashMap<>();
-    private final EventController eventController;
-    private final UserController userController;
     private String currentUser;
-    private final IGateway<User> userParser;
 
     public SystemController() {
         userParser = new UserParser("data/users.json");
-        this.userManager = new UserManager(userParser);
+        eventParser = new EventParser("data/events.json");
+        templateParser = new TemplateParser("data/templates.json");
 
-        this.templateManager = new TemplateManager();
-        this.eventManager = new EventManager(this.templateManager);
+        userManager = new UserManager(userParser);
+        eventManager = new EventManager(eventParser); // TODO fix constructor
+        templateManager = new TemplateManager(templateParser); // TODO fix constructor
 
-        this.presenter = new Presenter();
-        this.inputParser = new InputParser();
-        this.eventController = new EventController(this.userManager, this.eventManager, this.templateManager);
-        this.userController = new UserController(this.userManager, this.eventManager, this.templateManager);
+        presenter = new Presenter();
+        inputParser = new InputParser();
+
+        eventController = new EventController(userManager, eventManager, templateManager);
+        userController = new UserController(userManager, eventManager, templateManager);
+
+        initMenuMap();
+        // TODO run() was being called before, but i (hasti) feel like it's better to not call it
+    }
+
+    private void initMenuMap() {
         List<String> startupMenu = Arrays.asList("SignUp", "Login", "Trial");
-        List<String> mainMenu = Arrays.asList("Create Event", "View Events", "View My Events", "Account Menu", "Save");
+        List<String> mainMenu = Arrays.asList("Create Event", "View Events", "View My Events", "Account Menu", "Save",
+                "Go Back");
         List<String> trialMenu = Arrays.asList("Create Event", "View Events");
-        List<String> adminMenu = Arrays.asList("Create Event", "View Events", "View My Events", "Edit Template", "Account Menu", "Save");
-        List<String> accountMenu = Arrays.asList("Logout", "Change Username", "Change Password", "Change Email", "Change User Type", "Go Back", "Delete Account");
-        this.menuMap.put("Startup Menu", startupMenu);
-        this.menuMap.put("Main Menu", mainMenu);
-        this.menuMap.put("Account Menu", accountMenu);
-        this.menuMap.put("Trial Menu", trialMenu);
-        this.menuMap.put("Admin Menu", adminMenu);
-        this.run();
+        List<String> adminMenu = Arrays.asList("Create Event", "View Events", "View My Events", "Edit Template",
+                "Account Menu", "Save");
+        List<String> accountMenu = Arrays.asList("Logout", "Change Username", "Change Password", "Change Email",
+                "Change User Type", "Go Back", "Delete Account");
+        menuMap.put("Startup Menu", startupMenu);
+        menuMap.put("Main Menu", mainMenu);
+        menuMap.put("Account Menu", accountMenu);
+        menuMap.put("Trial Menu", trialMenu);
+        menuMap.put("Admin Menu", adminMenu);
     }
 
 
@@ -103,11 +113,37 @@ public class SystemController {
         }
     }
 
+    // "Create Event", "View Events", "View My Events", "Account Menu", "Save"
+    private boolean runMainMenu() {
+        boolean menu_running = true;
+        while (menu_running) {
+            String input = showMenu("Main Menu");
+            if (inputEquals(input, Arrays.asList("createevent", "create", "1"))) {
 
+            } else if (inputEquals(input, Arrays.asList("viewevents", "view", "viewall", "2"))) {
 
-//    public static void main(String[] args) {
-//        SystemController main = new SystemController();
-//        main.run();
-//    }
+            } else if (inputEquals(input, Arrays.asList("viewmyevents", "viewmy", "3"))) {
+
+            } else if (inputEquals(input, Arrays.asList("accountmenu", "account", "menu", "4"))) {
+
+            } else if (inputEquals(input, Arrays.asList("save", "5"))) {
+
+            } else {
+                // TODO
+            }
+        }
+    }
+
+    private String showMenu(String menuName) {
+        presenter.printMenu(menuName, menuMap.get(menuName));
+        return inputParser.readLine();
+    }
+
+    private boolean inputEquals(String input, List<String> prompts) {
+        input = input.toLowerCase();
+        input = input.trim(); // TODO maybe presenter should do this?
+        input = input.replace(" ", "");
+        return prompts.contains(input);
+    }
 }
 
