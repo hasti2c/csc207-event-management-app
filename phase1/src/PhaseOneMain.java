28251b833e84c1381631c7ef0e26893d5c89436f
Hasti Toossi <hasti.toossi@mail.utoronto.ca>
2021-07-22 22:11:38 -0400

    cleaned up TODOs in SystemController + changed PhaseOneMain

--- phase1/src/PhaseOneMain.java
+++ phase1/src/PhaseOneMain.java
@@ -1,5 +1,8 @@
+import controllersGatewaysPresenters.SystemController;
+
 public class PhaseOneMain {
     public static void main(String[] args) {
-
+        SystemController controller = new SystemController();
+        controller.run();
     }
 }
